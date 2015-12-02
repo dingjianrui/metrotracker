2df1a30aee9eb65ff09ef875119c5fc7c7766ea6
imJasonLiao <liaozhijia@126.com>
2015-12-02 22:04:33 +0800

    Updates: release 1 finished. login/register/marker functions

--- js/app.js
+++ js/app.js
@@ -14,16 +14,17 @@ function ViewModel() {
     self.registerEmail = ko.observable();
     self.registerPassword = ko.observable();
     self.registerPassword2 = ko.observable();
+    self.shouldShowMessage = ko.observable();
+    self.displayMessage = ko.observable();
 
 
-
-    if($.cookie('mt_userName')!=undefined){
+    if ($.cookie('mt_userName') != undefined) {
         $("#rememberMe").attr("checked", true);
         //read cookie
         self.loginEmail = ko.observable($.cookie('mt_userName'));
         self.loginPassword = ko.observable($.cookie('mt_password'));
         self.rememberMe = ko.observable('checked');
-    }else{
+    } else {
         $("#rememberMe").attr("checked", false);
         self.loginEmail = ko.observable();
         self.loginPassword = ko.observable();
@@ -31,11 +32,11 @@ function ViewModel() {
     }
 
     //remember me event
-    $("#rememberMe").click(function(){
-        if($('#rememberMe:checked').length>0){//set cookie
+    $("#rememberMe").click(function () {
+        if ($('#rememberMe:checked').length > 0) {//set cookie
             $.cookie('mt_userName', $('#username').val());
             $.cookie('mt_password', $('#password').val());
-        }else{//clear cookie
+        } else {//clear cookie
             $.removeCookie('mt_userName');
             $.removeCookie('mt_password');
         }
@@ -43,6 +44,11 @@ function ViewModel() {
 
     function showError(jqXHR) {
         self.result(jqXHR.status + ': ' + jqXHR.statusText);
+    }
+
+    function isValidEmailAddress(emailAddress) {
+        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+        return pattern.test(emailAddress);
     }
 
     self.callApi = function () {
@@ -65,6 +71,20 @@ function ViewModel() {
 
     self.register = function () {
         self.result('');
+        self.shouldShowMessage(false);
+        self.displayMessage('');
+
+        if(!isValidEmailAddress(self.registerEmail())){
+            self.shouldShowMessage(true);
+            self.displayMessage('Email is not valid!');
+            return;
+        }
+
+        if (self.registerPassword() != self.registerPassword2()) {
+            self.shouldShowMessage(true);
+            self.displayMessage('Password does not match!');
+            return;
+        }
 
         var data = {
             Email: self.registerEmail(),
@@ -74,7 +94,7 @@ function ViewModel() {
 
         $.ajax({
             type: 'POST',
-            url: apiUrl+'/api/Account/Register',
+            url: apiUrl + '/api/Account/Register',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function (data) {
@@ -98,7 +118,7 @@ function ViewModel() {
 
         $.ajax({
             type: 'POST',
-            url: apiUrl+'/Token',
+            url: apiUrl + '/Token',
             data: loginData,
             crossDomain: true
             //headers: headers1//('Access-Control-Allow-Origin: *')
